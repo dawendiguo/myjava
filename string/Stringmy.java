6953cbcbbab56a4d3026a47de20dbc2ac45d5549
dawendiguo <dawendiguo88@live.com>
2016-06-16 20:52:07 +0800

    delete stringfile

--- string/Stringmy.java
+++
@@ -1,9 +0,0 @@
-public class Stringmy{
-public static void main(String[] args){
-	String myname = "dawen";
-	String myname1 = "ddddddd";
-	myname += " ";
-	myname += "(" + myname1 + ")";
-	System.out.println(myname);
-}
-}
